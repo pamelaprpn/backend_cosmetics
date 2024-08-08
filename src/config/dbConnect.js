84ee2121dcff6f43e5613da1e0784213fa89a6ed
Pamela Raiane <pamelaprpn@gmail.com>
2024-08-08 14:04:08 -0300

    chore: config environment variable

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectaNaDatabase(){
 
-    mongoose.connect(process.env.DB_CONNECTION);
+    mongoose.connect(process.env.MONGODB_URI);
     return mongoose.connection;
 };
 
